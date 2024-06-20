ffcdb245b27a260aff79df592cb0f1551885e59a
SebastianRiveros <sriveros@unsa.edu.pe>
2024-06-19 22:12:35 -0500

    implementacion del metodo esRojo, para conocer el estado rojo o negro de un nodo

--- src/ArbolRojoNegro.java
+++ src/ArbolRojoNegro.java
@@ -1,7 +1,15 @@
 public class ArbolRojoNegro<T extends Comparable<T>> {
     private NodoRojoNegro<T> raiz;
-    
+
     public ArbolRojoNegro() {
         this.raiz = null;
+    }
+
+    //metodo auxiliar para conocer el estado rojo o negro del nodo
+    private boolean esRojo(NodoRojoNegro<T> nodo) {
+        if (nodo == null) {
+            return false;  //si el nodo es null, se considera negro (false)
+        }
+        return nodo.getEsRojo();  //devuelve el estado de color del nodo (true si es rojo, false si es negro)
     }
 }
