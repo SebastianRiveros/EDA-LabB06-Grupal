a48c2a962a1f89fad4e29b3bdcb10a38c4ce841b
SebastianRiveros <sriveros@unsa.edu.pe>
2024-06-19 22:50:47 -0500

    Implementacion del metodo rotarIzquierda, para hacer las rotaciones a la izquierda en nuetro arbol, la logica es similar que el metodo rotarDerecha por lo que solo cambiamos el sentido

--- src/ArbolRojoNegro.java
+++ src/ArbolRojoNegro.java
@@ -22,22 +22,37 @@ public class ArbolRojoNegro<T extends Comparable<T>> {
 
     //metodo auxiliar para aplicar rotaciones a la derecha
     private NodoRojoNegro<T> rotarDerecha(NodoRojoNegro<T> nodo) {
-        NodoRojoNegro<T> aux = nodo.getIzquierda();  // Guarda el hijo izquierdo en una variable temporal
+        NodoRojoNegro<T> aux = nodo.getIzquierda();
         nodo.setIzquierda(aux.getDerecha());  // El hijo derecho del hijo izquierdo pasa a ser el nuevo hijo izquierdo del nodo
-        if (aux.getDerecha() != null) {
+        if (aux.getDerecha() != null)
             aux.getDerecha().setPadre(nodo);  // Establece al nodo como padre del nuevo hijo izquierdo del nodo
-        }
         aux.setPadre(nodo.getPadre());  // El padre del nodo temporal se convierte en el padre del nodo actual
-        if (nodo.getPadre() == null) {
+        if (nodo.getPadre() == null) 
             raiz = aux;  // Si el nodo es la raíz, actualiza la raíz del árbol
-        } else if (nodo == nodo.getPadre().getDerecha()) {
+        else if (nodo == nodo.getPadre().getDerecha()) 
             nodo.getPadre().setDerecha(aux);  // Si el nodo es el hijo derecho de su padre, actualiza el hijo derecho del padre
-        } else {
+        else 
             nodo.getPadre().setIzquierda(aux);  // Si el nodo es el hijo izquierdo de su padre, actualiza el hijo izquierdo del padre
-        }
         aux.setDerecha(nodo);  // El nodo actual pasa a ser el hijo derecho del nodo temporal
         nodo.setPadre(aux);  // El padre del nodo actual se convierte en el nodo temporal
-        return aux;  // Retorna el nodo temporal, que ahora es la nueva raíz (en caso de rotaciones)
+        return aux;  // Retorna el nodo temporal, que ahora es la nueva raíz
     }
-
+ 
+    // metodo auxiliar para aplicar rotaciones a la izquierda
+    private NodoRojoNegro<T> rotarIzquierda(NodoRojoNegro<T> nodo) {
+        NodoRojoNegro<T> aux = nodo.getDerecha();  
+        nodo.setDerecha(aux.getIzquierda());  
+        if (aux.getIzquierda() != null) 
+            aux.getIzquierda().setPadre(nodo);  
+        aux.setPadre(nodo.getPadre());  
+        if (nodo.getPadre() == null) 
+            raiz = aux;  
+        else if (nodo == nodo.getPadre().getIzquierda()) 
+            nodo.getPadre().setIzquierda(aux);  
+        else 
+            nodo.getPadre().setDerecha(aux);  
+        aux.setIzquierda(nodo); 
+        nodo.setPadre(aux);  
+        return aux; 
+    }
 }
